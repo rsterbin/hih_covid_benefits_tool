582cada2b9453d6f3b9c06f6ff823f975adea3d1
Reha Sterbin <reha@omniti.com>
2020-05-10 14:26:13 -0400

    Added contact info to confirmation screen

---
+++ src/components/BenefitsTool/ContactInfo/ContactInfo.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './ContactInfo.css';
+
+const contactInfo = (props) => {
+    return (
+        <div className="ContactInfo">
+            <h3>Join Hand in Hand</h3>
+            <p class="invitation">Hand in Hand provides resources and support for employers of domestic workers. If you’d like to learn more, enter your info here.</p>
+            <form method="post" onSubmit={(e) => e.preventDefault()}>
+                <label>Email</label>
+                {props.emailError ?
+                    <div class="Error">We won't be able to contact you without a valid email address.</div>
+                : null}
+                <input type="text" name="email" value={props.email} onChange={props.emailChanged} autoComplete="email" />
+                <label>ZIP Code</label>
+                {props.zipError ?
+                   <div class="Error">Please enter a valid US ZIP code (55555 or 55555-4444).</div>
+                : null}
+                <input type="zip" name="zip" value={props.zip} onChange={props.zipChanged} autoComplete="current-zip" />
+            </form>
+        </div>
+    );
+}
+
+export default contactInfo;
--- src/containers/BenefitsTool/Confirmation/Confirmation.js
+++ src/containers/BenefitsTool/Confirmation/Confirmation.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import * as EmailValidator from 'email-validator';
 
 import AnswerList from '../../../components/BenefitsTool/AnswerList/AnswerList';
+import ContactInfo from '../../../components/BenefitsTool/ContactInfo/ContactInfo';
 import Controls from '../../../components/UI/Controls/Controls';
 import Api from '../../../utils/Api';
 
@@ -14,10 +16,35 @@ class Confirmation extends Component {
         zip: '',
         emailError: false,
         zipError: false,
-        saveError: false
+        saveError: false,
+        contactValidOrEmpty: false
     };
 
+    regexValidZip = /^[0-9]{5}(?:-[0-9]{4})?$/;
+
     questions = QuestionsData;
+
+    changeEmail = (e) => {
+        let val = e.target.value;
+        let newstate = { email: val };
+        if (val !== '' && !EmailValidator.validate(val)) {
+            newstate.emailError = true;
+        } else {
+            newstate.emailError = false;
+        }
+        this.setState(newstate, () => this.okToSubmitContact());
+    };
+
+    changeZip = (e) => {
+        let val = e.target.value;
+        let newstate = { zip: val };
+        if (val !== '' && !this.regexValidZip.test(val)) {
+            newstate.zipError = true;
+        } else {
+            newstate.zipError = false;
+        }
+        this.setState(newstate, () => this.okToSubmitContact());
+    };
 
     confirmAnswers = () => {
         const data = {
@@ -28,6 +55,14 @@ class Confirmation extends Component {
             const letter = this.props.answers[question];
             const answer = this.questions.spec[question].a[letter];
             data.answers[question] = answer.toUpperCase();
+        }
+        if (this.state.email.trim() !== '') {
+            data.contact = {
+                email: this.state.email.trim()
+            };
+            if (this.state.zip.trim() !== '') {
+                data.contact.zip = this.state.zip.trim();
+            }
         }
         this.setState({ loading: true });
         // TODO: Add email and zip intake
@@ -44,6 +79,22 @@ class Confirmation extends Component {
                 console.log(error);
             });
     };
+
+    okToSubmitContact() {
+        let validOrEmpty = null;
+        if (this.state.email === '' && this.state.zip === '') {
+            validOrEmpty = true;
+        } else if (this.state.email === '' || this.state.zip === '') {
+            validOrEmpty = false;
+        } else {
+            if (this.state.emailError || this.state.zipError) {
+                validOrEmpty = false;
+            } else {
+                validOrEmpty = true;
+            }
+        }
+        this.setState({ contactValidOrEmpty: validOrEmpty });
+    }
 
     render() {
         const buttons = [
@@ -69,6 +120,14 @@ class Confirmation extends Component {
                     questions={this.questions}
                     answers={this.props.answers}
                     edited={(q, a) => this.props.editAnswer(q, a)} />
+                <ContactInfo
+                    emailError={this.state.emailError}
+                    zipError={this.state.zipError}
+                    email={this.state.email}
+                    zip={this.state.zip}
+                    emailChanged={this.changeEmail}
+                    zipChanged={this.changeZip}
+                    />
                 <Controls
                     buttons={buttons}
                     links={links} />
