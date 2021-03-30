39ef750889a08a7a06e0ae02cb262e35f6c29d9d
Reha Sterbin <reha.sterbin@credativ.us>
2021-03-30 18:52:35 -0400

    Manage the legend independently so that charts can be the same size

--- src/components/Admin/Charts/Pie/Pie.js
+++ src/components/Admin/Charts/Pie/Pie.js
@@ -1,40 +1,34 @@
 import React from 'react';
 import { ResponsivePie } from '@nivo/pie';
 
+import './Pie.css';
+
 const pieChart = (props) => {
 
-    let height = 250;
-    let margin = 30;
-
-    height += (props.data.length * 17);
-    margin += (props.data.length * 17);
-    console.log('height', height);
-    console.log('margin', margin);
-
     return (
-        <div style={{ height: height + 'px' }}>
-            <ResponsivePie data={props.data}
-                margin={{ top: 20, right: 40, bottom: margin, left: 40 }}
-                innerRadius={0.5}
-                padAngle={0.7}
-                cornerRadius={3}
-                colors={{ scheme: 'paired' }}
-                borderWidth={0.5}
-                borderColor={{ from: 'color', modifiers: [ [ 'darker', 0.2 ] ] }}
-                enableRadialLabels={false}
-                legends={[
-                    {
-                        anchor: 'bottom',
-                        direction: 'column',
-                        translateX: margin * -1,
-                        translateY: margin,
-                        itemsSpacing: 0,
-                        itemWidth: 100,
-                        itemHeight: 18,
-                        symbolShape: 'circle',
-                    }
-                ]}
-                />
+        <div className="PieChart">
+            <div className="Diagram" style={{ height: '250px' }} key="diagram">
+                <ResponsivePie data={props.data}
+                    margin={{ top: 20, right: 40, bottom: 20, left: 40 }}
+                    innerRadius={0.5}
+                    padAngle={0.7}
+                    cornerRadius={3}
+                    colors={{ datum: 'data.color' }}
+                    borderWidth={0.5}
+                    borderColor={{ from: 'color', modifiers: [ [ 'darker', 0.2 ] ] }}
+                    enableRadialLabels={false}
+                    />
+            </div>
+            <div className="Legend" key="legend">
+                {props.data.map((item, idx) =>
+                    <div className="Item" key={'item-' + idx}>
+                        <div key="dot" className="Cell Dot">
+                            <span className="InnerDot" style={{ backgroundColor: item.color }}></span>
+                        </div>
+                        <div key="label" className="Cell Label">{item.label}</div>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
--- src/utils/dashboard.js
+++ src/utils/dashboard.js
@@ -1,14 +1,30 @@
+import { schemeSet3, schemeGreys } from 'd3-scale-chromatic';
+import { shuffle } from './utils';
 
 export const statsToChartData = (stats) => {
     if (typeof stats != 'object') {
         stats = {};
     }
     const cdata = [];
+    let scheme = [];
+    for (let i = 0; i < 12; ++i) {
+        scheme.push(schemeSet3[i]);
+    }
+    if (Object.keys(stats).length > scheme.length) {
+        const count = Object.keys(stats).length - scheme.length;
+        const extraScheme = schemeGreys[count + 2];
+        for (let k = 0; k < count; ++k) {
+            scheme.push(extraScheme[k]);
+        }
+    }
+    const schemeRandom = shuffle(scheme);
+    let idx = 0;
     for (const key in stats) {
         cdata.push({
             id: key,
             label: key,
             value: stats[key],
+            color: schemeRandom[idx++],
         });
     }
     if (cdata.length < 1) {
